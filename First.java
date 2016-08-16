020b8603765757c258f532a8474c43eaff142a46
torpeda4ka <danil1200@ukr.net>
2016-08-16 15:25:59 +0300

    Add files via upload

---
+++ First.java
@@ -0,0 +1,16 @@
+
+public class First {
+  
+  int i;
+  char c;
+  
+  
+  public static void main(String[] args) { 
+    First a = new First();
+    System.out.println(a.c);
+    System.out.println(a.i);
+  }
+  
+ 
+  
+}
