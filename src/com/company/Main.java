83413ca6fcc91938d5ced634d523905b26d2d14a
torpeda4ka <danil1200@ukr.net>
2016-08-28 00:38:47 +0300

    first task

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+    static int i;
+    static char c;
+    public static void main(String[] args) {
+        System.out.println(i);
+        System.out.println(c);
+
+    }
+}
